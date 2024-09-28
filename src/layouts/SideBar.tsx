5797bdda0cb12b48fd1b1c05605e3fa05ebff135
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-29 00:15:48 +0100

    chores: added some css

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,5 +1,9 @@
 const SideBar = () => {
-  return <div>Menu</div>;
+  return (
+    <aside className="sticky top-0 overflow-y-auto scrollbar-hidden">
+      Menu
+    </aside>
+  );
 };
 
 export default SideBar;
