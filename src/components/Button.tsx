d8b2c508c2878644081af82505e7480eb6c52b80
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-28 12:51:05 +0100

    feat: added variants for the button classes for consistenncy

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,38 @@
-import { cva } from "class-variance-authority";
+import { cva, VariantProps } from "class-variance-authority";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-const buttonStyles = cva;
+const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"],
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
-const Button = () => {
-  return <button />;
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
+
+const Button = ({ variant, size, className, ...props }: ButtonProps) => {
+  return (
+    <button className={twMerge(buttonStyles({ variant, size }), className)} />
+  );
 };
 
 export default Button;
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,13 +1,14 @@
 import { Menu } from "lucide-react";
 import logo from "../assets/Logo.png";
+import Button from "../components/Button";
 
 const PageHeader = () => {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <button>
+        <Button>
           <Menu />
-        </button>
+        </Button>
         <a href="/">
           <img src={logo} className="h-6" />
         </a>
