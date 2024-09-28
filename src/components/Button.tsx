fd89418870384e1b014798423aa21213cb113c13
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-28 12:17:03 +0100

    feat: install the cva package created a button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { cva } from "class-variance-authority";
+
+const buttonStyles = cva;
+
+const Button = () => {
+  return <button />;
+};
+
+export default Button;
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,5 +1,21 @@
+import { Menu } from "lucide-react";
+import logo from "../assets/Logo.png";
+
 const PageHeader = () => {
-  return <div>PageHeader</div>;
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <button>
+          <Menu />
+        </button>
+        <a href="/">
+          <img src={logo} className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default PageHeader;
