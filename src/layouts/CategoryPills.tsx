0f52d20c170ad67e574739952c0a5db5ac30fed1
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-28 19:35:36 +0100

    feat: added functionalities to the buttons on the category component

--- src/layouts/CategoryPills.tsx
+++ src/layouts/CategoryPills.tsx
@@ -1,6 +1,6 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Button from "../components/Button";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type CategoryPillProps = {
   categories: string[];
@@ -8,17 +8,44 @@ type CategoryPillProps = {
   onSelect: (category: string) => void;
 };
 
+const TRANSLATE_AMOUNT = 200;
+
 const CategoryPills = ({
   categories,
   selectedCategory,
   onSelect,
 }: CategoryPillProps) => {
+  const [translate, setTranslate] = useState(0);
   const [isLeftVisible, setIsLeftVisible] = useState(false);
-  const [isRightVisible, setIsRightVisible] = useState(true);
+  const [isRightVisible, setIsRightVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current === null) return;
+
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0]?.target;
+      if (container == null) return;
+
+      setIsLeftVisible(translate > 0);
+      setIsRightVisible(
+        translate + container.clientWidth < container.scrollWidth
+      );
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [categories, translate]);
 
   return (
-    <div className="overflow-x-hidden relative ">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+    <div ref={containerRef} className="overflow-x-hidden relative">
+      <div
+        className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map((category) => (
           <Button
             onClick={() => onSelect(category)}
@@ -36,6 +63,13 @@ const CategoryPills = ({
             variant="ghost"
             size="icon"
             className="h-full aspect-square w-auto p-1.5"
+            onClick={() =>
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_AMOUNT;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              })
+            }
           >
             <ChevronLeft />
           </Button>
@@ -47,6 +81,18 @@ const CategoryPills = ({
             variant="ghost"
             size="icon"
             className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                if (containerRef.current == null) return translate;
+                const newTranslate = translate + TRANSLATE_AMOUNT;
+                const edge = containerRef.current.scrollWidth;
+                const width = containerRef.current.clientWidth;
+                if (newTranslate + width >= edge) {
+                  return edge - width;
+                }
+                return newTranslate;
+              });
+            }}
           >
             <ChevronRight />
           </Button>
