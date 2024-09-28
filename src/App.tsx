864ee477fdc3516d43bf14f29946c60415197159
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-28 12:03:20 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import PageHeader from "./layouts/PageHeader";
+
+const App = () => {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div>1</div>
+      <div>2</div>
+    </div>
+  );
+};
+
+export default App;
