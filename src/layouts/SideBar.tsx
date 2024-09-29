5204b9635e9dbccda7ffdbcac756043353e2301d
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-29 18:16:49 +0100

    added the mobile view collapsed side Icons

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,9 +1,42 @@
+import { Clapperboard, Home, Library, Repeat } from "lucide-react";
+import { ElementType } from "react";
+import { buttonStyles } from "../components/Button";
+import { twMerge } from "tailwind-merge";
+
 const SideBar = () => {
   return (
-    <aside className="sticky top-0 overflow-y-auto scrollbar-hidden">
-      Menu
+    <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+      <SmallSidebarItem Icon={Home} title="Home" url="/" />
+      <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+      <SmallSidebarItem
+        Icon={Clapperboard}
+        title="Subscriptions"
+        url="/subscriptions"
+      />
+      <SmallSidebarItem Icon={Library} title="Library" url="/library" />
     </aside>
   );
 };
 
 export default SideBar;
+
+type SmallSideBarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+};
+
+function SmallSidebarItem({ Icon, title, url }: SmallSideBarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        "py-4 px-1 flex flex-col items-center rounded-lg"
+      )}
+    >
+      <Icon className="w-6 h-6" />
+      <div className="text-sm">{title}</div>
+    </a>
+  );
+}
